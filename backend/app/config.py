41b6f7f4f4f9fd2462d581fc45bc70a38f4c7022
danny2708 <duyanh27082005@gmail.com>
2025-08-14 00:42:35 +0700

    add enpoint for gg sheet import user

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
 import os
+import backend
 
 load_dotenv(dotenv_path="backend/credentials.env")
+
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+google_cred_path = os.path.join(os.getcwd(), GOOGLE_APPLICATION_CREDENTIALS)
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 from datetime import date, datetime
 
 class UserBase(BaseModel):
     username: str = Field(..., example="john_doe")
-    email: Optional[str] = Field(None, example="john.doe@example.com")
+    email: Optional[EmailStr] = Field(None, example="john.doe@example.com")
     full_name: Optional[str] = Field(None, example="John Doe")
     date_of_birth: Optional[date] = Field(None, example="1990-01-01")
     gender: Optional[str] = Field(None, example="Male")
@@ -15,7 +15,7 @@ class UserCreate(UserBase):
 
 class UserUpdate(UserBase):
     username: Optional[str] = None
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
     full_name: Optional[str] = None
     date_of_birth: Optional[date] = None
     gender: Optional[str] = None
@@ -28,3 +28,8 @@ class User(UserBase):
     
     class Config:
         from_attributes = True
+
+class SheetUserCreate(BaseModel):
+    username: str
+    full_name: str
+    email: EmailStr
