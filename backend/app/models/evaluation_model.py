e382dfaa24fb0cf8df91d2772842e6628745feb3
danny2708 <duyanh27082005@gmail.com>
2025-08-22 12:23:58 +0700

    finish evaluation service

--- backend/app/models/evaluation_model.py
+++ backend/app/models/evaluation_model.py
@@ -11,9 +11,12 @@ class Evaluation(Base):
     evaluation_id = Column(Integer, primary_key=True)
     student_id = Column(Integer, ForeignKey('students.student_id'), nullable=False)
     teacher_id = Column(Integer, ForeignKey('teachers.teacher_id'), nullable=False)
-    evaluation_type = Column(Text)
+    evaluation_type = Column(Text, nullable=False)
     evaluation_date = Column(Date, nullable=False)
-
+    study_point = Column(Integer, nullable=False)
+    discipline_point = Column(Integer, nullable=False)
+    evaluation_content = Column(Text, nullable=False)
+    
     # Mối quan hệ với học sinh và giáo viên (many-to-one)
     student = relationship("Student", back_populates="evaluations")
     teacher = relationship("Teacher", back_populates="evaluations")
